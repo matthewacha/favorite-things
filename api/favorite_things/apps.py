a5d4e1d076316929b20d16f2310c327bf480fbe6
matthewacha <matthewacha@gmail.com>
2019-06-21 21:00:27 +0300

    ch(setup) Setup dev environment - setup django app -setup test environment -setup deployment

---
+++ api/favorite_things/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FavoriteThingsConfig(AppConfig):
+    name = 'favorite_things'
---
+++ quiz.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+
+key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
+
+# Oh no! The code is going over the edge! What are you going to do?
+message = b'gAAAAABdDNNl6XK32g_6dHpesU0QuYpHEXXdPcEPEDlsGUD_7VNBdc-8jk61LcaU75D5deKMWbtKMk-xY1GA81liMia2ES9U-UuFcvXxmNib0uYDTk4JdZMD4hpOYGAFBsA5jLbXOcTi5KqBM5SqAvPQT-JqMWvGKyWLwAk7pJzEM0Y6gV5WPmQ='
+
+def main():
+    f = Fernet(key)
+    print(f.decrypt(message))
+
+
+if __name__ == "__main__":
+    main()
