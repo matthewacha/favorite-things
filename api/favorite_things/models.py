2d2211a5428782c3c77c0f37c57029adcd26fdd9
matthewacha <matthewacha@gmail.com>
2019-06-22 00:11:51 +0300

    ft(serializer) Add serializer - add user serializer - add favorite serializer

--- api/favorite_things/models.py
+++ api/favorite_things/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Favorite(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.CharField(max_length=255, null=False)
+    ranking = models.IntegerField(null=False, blank=False)
+    category =  models.CharField(max_length=255, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    audit_log = models.CharField(max_length=350, null=False, blank=False)
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.title, self.ranking, self.category)
---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from favorite_things.models import Favorites
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+class FavoriteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ('url', 'name', 'description', 'ranking', 'Metadata',
+        'category', 'created_at', 'modified_date', 'audit_log')
